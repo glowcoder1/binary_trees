6076630ed506a3f769daad812a009b4ba951449c
root <tefeglory.official@gmail.com>
2024-01-05 00:05:10 +0100

    task  13

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -15,7 +15,7 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 		return (0);
 
 	if (tree->left || tree->right)
-		nodest = 1;
+		nodes = 1;
 
 	nodes += binary_tree_nodes(tree->left);
 	nodes += binary_tree_nodes(tree->right);
